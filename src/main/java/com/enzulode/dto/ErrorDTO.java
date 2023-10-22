8ad6bd6ee32cfadce5a7ea8dc94a53635a1b987a
Vladislav Andreev <andreevvlad443@gmail.com>
2023-10-22 17:13:03 +0300

    feat: error page content DTO

---
+++ src/main/java/com/enzulode/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package com.enzulode.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+public class ErrorDTO
+{
+	private final String message;
+}
