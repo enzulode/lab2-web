fa3facc1464e7454766ae73b4f2330d69b29259d
Vladislav Andreev <andreevvlad443@gmail.com>
2023-10-22 21:24:48 +0300

    feat: form data validation implemented

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,3 +1,24 @@
+import {applyErrorMessage, clearErrors, parseAppropriateFloat, validateFormData} from "./utils/Utils.js";
+
 $(document).ready(() => {
-// 	TODO: implement logic here
+	const form = $("form")
+
+	// perform form validation and submit form if form data is ok
+	function validateInputs() {
+		let xValue = parseAppropriateFloat($("#x-selection :selected").val())
+		let yValue = parseAppropriateFloat($("#y-selection").val())
+		let rValue = parseAppropriateFloat($("input[name='r']").val())
+
+		let validationResult = validateFormData({x: xValue, y: yValue, r: rValue})
+		if (!validationResult.result)
+		{
+			applyErrorMessage(validationResult.message)
+			return
+		}
+
+		clearErrors()
+		form.submit()
+	}
+
+	$("#do-check").click((e) => validateInputs())
 })
