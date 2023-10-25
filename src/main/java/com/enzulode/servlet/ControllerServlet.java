582f869477cc1693935719dcaf439da5efced07c
Vladislav Andreev <andreevvlad443@gmail.com>
2023-10-25 09:20:58 +0300

    feat: controller servlet implemented

---
+++ src/main/java/com/enzulode/servlet/ControllerServlet.java
@@ -0,0 +1,36 @@
+package com.enzulode.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * This servlet is responsible for the whole application requests dispatching.
+ *
+ */
+public class ControllerServlet extends HttpServlet
+{
+
+	/**
+	 * This method handles GET requests.
+	 *
+	 * @param req an {@link HttpServletRequest} object that contains the request the client has made of the servlet
+	 *
+	 * @param resp an {@link HttpServletResponse} object that contains the response the servlet sends to the client
+	 *
+	 * @throws ServletException if the request for the GET could not be handled
+	 * @throws IOException if an input or output error is detected when the servlet handles the GET request
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		if (req.getParameter("x") == null || req.getParameter("y") == null
+				|| req.getParameter("r") == null)
+			getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+		else
+			getServletContext().getRequestDispatcher("/check").forward(req, resp);
+	}
+}
