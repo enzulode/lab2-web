f5384ef5adfc198a70c09a7dbc2c8a7df9421fdd
Vladislav Andreev <andreevvlad443@gmail.com>
2023-11-08 11:42:55 +0300

    feat: mainpage-related code refactorings & backbutton traversal detection

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -1,17 +1,60 @@
 import {applyErrorMessage, clearErrors, parseAppropriateFloat, validateFormData} from "./utils/Utils.js";
 import {Canvas} from "./utils/Canvas.js";
 
-const canvas = new Canvas("area")
-
-$(window).on('load', () => {
-	canvas.drawEmptyArea()
-})
-
 $(document).ready(() => {
 	const form = $("form")
+	const canvas = new Canvas("area")
 
-	// perform form validation and submit form if form data is ok
-	function validateInputs() {
+	$(window).on('pageshow', (e) => {
+		let historyTraversal = e.persisted
+			|| (typeof window.performance != 'undefined' && window.performance.navigation.type === 2)
+
+		if (historyTraversal)
+			window.location.reload()
+	})
+
+	calculatedPoints.forEach((point) => {
+		$('#results-container').append(
+			`<tr>
+				<td>${point.x}</td>
+				<td>${point.y}</td>
+				<td>${point.r}</td>
+				<td>${point.resultString}</td>
+			</tr>`
+		)
+	})
+
+	canvas.drawEmptyArea()
+
+	$("input[type='radio'][name='r']").click((e) => {
+		canvas.redrawArea($(e.target).val(), true)
+		canvas.applyPoints(calculatedPoints)
+	})
+
+	canvas.onClick(
+		(success) => {
+			clearErrors()
+			performCalculation(success)
+		},
+		(error) => applyErrorMessage(error)
+	)
+
+	$("#do-check").click((e) => processForm())
+
+
+	/*  --------------------------------------------------------------------
+		--------------------------------------------------------------------
+		||||                                                            ||||
+		||||                    Function declaration                    ||||
+		||||                                                            ||||
+		--------------------------------------------------------------------
+		--------------------------------------------------------------------    */
+
+	/**
+	 * Performs form validation and if it succeeds - performs form submit.
+	 *
+	 */
+	function processForm() {
 		let xValue = parseAppropriateFloat($("#x-selection :selected").val())
 		let yValue = parseAppropriateFloat($("#y-selection").val())
 		let rValue = parseAppropriateFloat($("input[name='r']").val())
@@ -25,25 +68,18 @@ $(document).ready(() => {
 
 		clearErrors()
 		form.submit()
+		form.reset()
 	}
 
-	$("#do-check").click((e) => validateInputs())
-	$("input[name='r']").click((e) => {
-		canvas.redrawArea($(e.target).val(), true)
-		canvas.applyPoints(calculatedPoints)
-	})
-
-	canvas.onClick(
-		(success) => {
-			clearErrors()
-
-			let loc = window.location
-			let calculateUrl = `${loc.protocol}//${loc.host}${loc.pathname}?x=${success.x}&y=${success.y}&r=${success.r}`
-			$(location).prop('href', calculateUrl)
-		},
-		(error) => {
-			applyErrorMessage(error)
-		}
-	)
+	/**
+	 * Performs exact calculation redirect for point check.
+	 *
+	 * @param data data to be passed with the request
+	 */
+	function performCalculation(data) {
+		let loc = window.location
+		let calculateUrl = `${loc.protocol}//${loc.host}${loc.pathname}?x=${data.x}&y=${data.y}&r=${data.r}`
+		$(location).prop('href', calculateUrl)
+	}
 
 })
